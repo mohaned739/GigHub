bdc4913994932b9da0d8c7a5b8038cf06a54de4a
Mohaned739 <mohaned.mohamed9999@gmail.com>
2022-08-28 00:27:10 +0200

    Create Add a Gig Form

---
+++ Controllers/GigsController.cs
@@ -0,0 +1,22 @@
+﻿using GigHub2.Models;
+using GigHub2.ViewModels;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace GigHub2.Controllers
+{
+    public class GigsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        public GigsController()
+        {
+            _context = new ApplicationDbContext();
+        }
+        public ActionResult Create()
+        {
+            var viewModel = new GigFormViewModel { Genres = _context.Genres.ToList()};
+
+            return View(viewModel);
+        }
+    }
+}
---
+++ ViewModels/GigFormViewModel.cs
@@ -0,0 +1,14 @@
+﻿using GigHub2.Models;
+using System.Collections.Generic;
+
+namespace GigHub2.ViewModels
+{
+    public class GigFormViewModel
+    {
+        public string Venue { get; set; }
+        public string Date { get; set; }
+        public string Time { get; set; }
+        public int Genre { get; set; }
+        public IEnumerable<Genre> Genres { get; set; }
+    }
+}
