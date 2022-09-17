7280e4c77df6bf99173ae4ea8a7c1028509253ff
Mohaned739 <mohaned.mohamed9999@gmail.com>
2022-09-17 15:47:27 +0200

    Improve the look and feel of the home page

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -16,7 +16,10 @@ namespace GigHub2.Controllers
         }
         public ActionResult Index()
         {
-            var upcomingGigs = _context.Gigs.Include(g => g.Artist).Where(g=>g.DateTime > DateTime.Now);
+            var upcomingGigs = _context.Gigs
+                                .Include(g => g.Artist)
+                                .Include(g=> g.Genre)
+                                .Where(g=>g.DateTime > DateTime.Now);
             return View(upcomingGigs);
         }
 
